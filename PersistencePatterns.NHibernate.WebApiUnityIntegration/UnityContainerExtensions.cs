c00a870c593e286517e9fd12401bbe445be8d869
dgarcia202 <wft2000@gmail.com>
2017-03-05 12:37:38 +0100

    webapi unity integration

---
+++ PersistencePatterns.NHibernate.WebApiUnityIntegration/UnityContainerExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace PersistencePatterns.NHibernate.WebApiUnityIntegration
+{
+    using System.Reflection;
+
+    using Microsoft.Practices.Unity;
+
+    using PersistencePatterns.Contracts;
+
+    public static class UnityContainerExtensions
+    {
+        public static IUnityContainer RegisterPersistencePatterns(this IUnityContainer container, string connectionString, Assembly mappingsAssembly)
+        {
+            return container
+                .RegisterType<ISessionProvider, OracleSessionProvider>(new ContainerControlledLifetimeManager(), new InjectionConstructor(connectionString, mappingsAssembly))
+                .RegisterType<IUnitOfWork, IUnitOfWork>(new HierarchicalLifetimeManager());
+        }
+    }
+}
