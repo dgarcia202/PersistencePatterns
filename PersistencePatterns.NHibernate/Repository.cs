eb90d015e7ec1c17af461e1b16772e805ec508d6
dgarcia202 <wft2000@gmail.com>
2017-03-05 11:32:50 +0100

    repo implementation

--- PersistencePatterns.NHibernate/Repository.cs
+++ PersistencePatterns.NHibernate/Repository.cs
@@ -1,15 +1,69 @@
 ﻿namespace PersistencePatterns.NHibernate
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
+
+    using global::NHibernate.Linq;
 
     using PersistencePatterns.Contracts;
 
     public class Repository<TKey, TEntity> : IRepository<TKey, TEntity>
         where TEntity : class, new()
     {
-        public Type ElementType => typeof(TEntity);
+        private readonly UnitOfWork unitOfWork;
 
-        public Expression Expression => 
+        public Repository(IUnitOfWork unitOfWork)
+        {
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWork");
+            }
+
+            this.unitOfWork = unitOfWork as UnitOfWork;
+
+            if (this.unitOfWork == null)
+            {
+                throw new ArgumentException("Repository received an invalid implementation of IUnitOfWork.");
+            }
+        }
+
+        public Type ElementType => this.unitOfWork.Session.Query<TEntity>().ElementType;
+
+        public Expression Expression => this.unitOfWork.Session.Query<TEntity>().Expression;
+
+        public IQueryProvider Provider => this.unitOfWork.Session.Query<TEntity>().Provider;
+
+        public void Add(TEntity entity)
+        {
+            this.unitOfWork.Session.Save(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            this.unitOfWork.Session.Merge(entity);
+        }
+
+        public TEntity Get(TKey id)
+        {
+            return this.unitOfWork.Session.Get<TEntity>(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            this.unitOfWork.Session.Delete(entity);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return this.unitOfWork.Session.Query<TEntity>().GetEnumerator();
+        }
     }
 }
