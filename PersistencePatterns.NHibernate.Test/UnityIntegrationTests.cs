da02e5468f2c8aebacd459c7fa5ed1c36d0848c9
dgarcia202 <wft2000@gmail.com>
2017-03-05 13:05:19 +0100

    unity integration tests

---
+++ PersistencePatterns.NHibernate.Test/UnityIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿namespace PersistencePatterns.NHibernate.Test
+{
+    using System.Configuration;
+    using System.Linq;
+
+    using Microsoft.Practices.Unity;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using PersistencePatterns.Contracts;
+    using PersistencePatterns.NHibernate.WebApiUnityIntegration;
+
+    [TestClass]
+    public class UnityIntegrationTests
+    {
+        [TestMethod]
+        public void SessionProviderIsRegisteredAsSingletonTest()
+        {
+            // arrange
+            var container = new UnityContainer();
+
+            // act
+            container.RegisterPersistencePatterns(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString(), this.GetType().Assembly);
+            var registration = container.Registrations.FirstOrDefault(x => x.RegisteredType == typeof(ISessionProvider));
+
+            // assert
+            Assert.IsNotNull(registration);
+            Assert.IsTrue(registration.LifetimeManager is ContainerControlledLifetimeManager);
+        }
+
+        [TestMethod]
+        public void UnitOfWorkIsRegisteredWithHierarchical()
+        {
+            // arrange
+            var container = new UnityContainer();
+
+            // act
+            container.RegisterPersistencePatterns(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString(), this.GetType().Assembly);
+            var registration = container.Registrations.FirstOrDefault(x => x.RegisteredType == typeof(IUnitOfWork));
+
+            // assert
+            Assert.IsNotNull(registration);
+            Assert.IsTrue(registration.LifetimeManager is HierarchicalLifetimeManager);
+        }
+    }
+}
